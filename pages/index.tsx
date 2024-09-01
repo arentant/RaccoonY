aef70b2453b581178b1b561f98697e82531e9c75
Aren <aren@bransfer.io>
2024-09-01 19:50:01 +0400

    update link contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className={`flex flex-col items-center gap-3 h-screen w-full relative`}>
         <div className="h-full flex flex-col items-center justify-center gap-3">
           <Image src={"/logo.png"} alt={"logo"} width={500} height={500} />
-          <Link href={'/11155111/0xa2efA3AF381E7eCCd77EB738D309b0fF2F7F4614'} className="px-5 py-2.5 text-xl mt-2 bg-accent-foreground rounded-xl text-accent active:scale-90 hover:brightness-90 duration-100 transition-all">
+          <Link href={'/11155111/0x1488024224fbca31d20f8bde51c8c1365fc2efe7'} className="px-5 py-2.5 text-xl mt-2 bg-accent-foreground rounded-xl text-accent active:scale-90 hover:brightness-90 duration-100 transition-all">
             Start game
           </Link>
         </div>
