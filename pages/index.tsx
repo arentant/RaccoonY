2f882b92f246c3ac4e1994544e022df3d9f368cd
Aren <aren@bransfer.io>
2024-09-01 22:32:40 +0400

    update abi and contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className={`flex flex-col items-center gap-3 h-screen w-full relative`}>
         <div className="h-full flex flex-col items-center justify-center gap-3">
           <Image src={"/logo.png"} alt={"logo"} width={500} height={500} />
-          <Link href={'/11155111/0x185ddAedD574D056CccC97618A6aA824597F97b1'} className="px-5 py-2.5 text-xl mt-2 bg-accent-foreground rounded-xl text-accent active:scale-90 hover:brightness-90 duration-100 transition-all">
+          <Link href={'/11155111/0xa0347807707F4BFD8890361eD3D1D3d880E5397e'} className="px-5 py-2.5 text-xl mt-2 bg-accent-foreground rounded-xl text-accent active:scale-90 hover:brightness-90 duration-100 transition-all">
             Start game
           </Link>
         </div>
